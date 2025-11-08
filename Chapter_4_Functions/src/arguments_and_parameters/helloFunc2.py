f3dd14b08c8adf1e0d74681fa74c9877e5b25ddc
Cesar David Millan <cesardavidmillan5@gmail.com>
2025-11-08 18:11:59 -0500

    Working on chapter 4 on functions

---
+++ Chapter_4_Functions/src/arguments_and_parameters/helloFunc2.py
@@ -0,0 +1,8 @@
+def say_hello_to(name):
+    """Prints three greetings to the name provided"""
+    print(f"Good Morning, {name}")
+    print(f"Good Afternoon, {name}")
+    print(f"Good Evening, {name}")
+
+say_hello_to('Alice')
+say_hello_to('Bob')
---
+++ Chapter_4_Functions/src/creating_functions/helloFunc.py
@@ -0,0 +1,10 @@
+def hello():
+    """Print three greetings"""
+    print('Good Morning!')
+    print('Good Afternoon!')
+    print('Good Evening!')
+
+hello()
+hello()
+print('ONE MORE TIME!')
+hello()
